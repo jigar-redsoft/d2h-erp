e58ed0002dac4cecb294ae41cf036d4f80961e7f
Ashish Baravaliya <vier.institute35@gmail.com>
2024-09-19 23:00:22 -0400

    feat: complaint form added

--- d2h/d2h/doctype/complaint_form/complaint_form.js
+++ d2h/d2h/doctype/complaint_form/complaint_form.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, ashish and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Complaint Form", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Complaint Form", {
+  warranty_expiry_date: function (frm) {
+    const today = frappe.datetime.nowdate();
+    if (today > frm.doc.warranty_expiry_date) {
+      frappe.msgprint("The warranty period has expired.");
+      frm.set_value("warranty_status", "True");
+    }
+  },
+});
