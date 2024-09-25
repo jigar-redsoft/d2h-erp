96d27e6687260b4b8425d5efce8afc6c88b5431d
Ashish Baravaliya <vier.institute35@gmail.com>
2024-09-24 23:05:59 -0400

    feat: job card

--- d2h/d2h/doctype/job_cards/job_cards.js
+++ d2h/d2h/doctype/job_cards/job_cards.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2024, ashish and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Job Cards", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Job Cards", {
+  get_equipments: function (frm) {
+    if (frm.doc.activities) {
+      frappe.call({
+        method: "d2h.d2h.doctype.job_cards.job_cards.get_equipments",
+        args: {
+          activities: frm.doc.activities.map((x) => x.activity),
+        },
+        callback: function (r) {
+          if (r.message) {
+            frm.clear_table("activity_equipments");
+            r.message.forEach((element) => {
+              frm.add_child("activity_equipments", {
+                equipment: element.equipment,
+                quantity: element.quantity,
+              });
+            });
+            frm.refresh_field("activity_equipments");
+          }
+        },
+      });
+    }
+  },
+});
--- d2h/d2h/doctype/job_cards/job_cards.py
+++ d2h/d2h/doctype/job_cards/job_cards.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2024, ashish and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+import json
 
 class JobCards(Document):
 	pass
+
+@frappe.whitelist()
+def get_equipments(activities):
+	activities = json.loads(activities)
+	data = []
+	for activity in activities:
+		equipments = frappe.get_all("Activity Equipment", filters={"parent": activity}, fields=["equipment", "quantity"])
+		for equipment in equipments:
+			data.append({
+				"equipment": equipment.equipment,
+				"quantity": equipment.quantity
+			})
+	return data
